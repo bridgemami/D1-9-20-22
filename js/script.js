d0c0c15eebf73f2ccf895c6c81dc400dfcaf5b64
bridgemami <mdbridgeman@gmail.com>
2022-09-22 18:11:59 -0700

    new branch

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 /* hello
 -
 world*/
-
-console.log('Michael Bridgeman');
-console.log('Salad');
+const mike= 'Michael Bridgeman';
+console.log(mike);
+const salad='salad';
+console.log(salad);
 //primitive data structure
 var string = 'hello world';
 var number = Number('5');
@@ -16,6 +17,7 @@ var object = {"word": "hello",
             "boolean": false};
 var undef =undefined;
 var nulls = null;
+
 var func = function add (a,b) {return a + b};
 console.log(string + '\n' + number + '\n' + boolean + '\n' + array + '\n' + object + '\n' + + undef + '\n' + nulls);
 console.log(array[1])
